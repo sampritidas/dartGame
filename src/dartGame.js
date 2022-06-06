89ef6dd8116120c8e62786fa705f13e2513966ab
Sampriti Das <99536474+sampritidas@users.noreply.github.com>
2022-06-06 12:22:33 +0530

    Added Game and Board class and Tested Board method

--- src/dartGame.js
+++ src/dartGame.js
@@ -1,3 +1,43 @@
+const scoreChart = {
+  '0': 100,
+  '1': 80, '2': 80,
+  '3': 60, '4': 60,
+  '5': 40, '6': 40,
+  '7': 20, '8': 20,
+  '9': 10, '10': 10,
+  '11 - rest': 0,
+};
+
+// const randomInt = () => Math.floor(Math.random() * 12);
+
+const getNewPoint = () => {
+  const x = 5;
+  const y = 6;
+  return { x, y };
+};
+
+class Board {
+  constructor() {
+    this.center = { x: 10, y: 10 };
+    this.radius = 10;
+  }
+
+  distanceFromCenter(newPoint) {
+    const dx = newPoint.x - this.center.x;
+    const dy = newPoint.y - this.center.y;
+    return Math.round(Math.hypot(dx, dy));
+  }
+
+  scoreBasedOnDistance(distance) {
+    return scoreChart[distance];
+  }
+
+  getScore(newPoint) {
+    const distance = this.distanceFromCenter(newPoint);
+    return this.scoreBasedOnDistance(distance);
+  }
+}
+
 class Game {
   constructor(board) {
     this.board = board;
@@ -6,7 +46,9 @@ class Game {
 
   play(totalRounds) {
     while (totalRounds > 0) {
-      const newScore = 5;
+      console.log('chance left', totalRounds);
+      const newPoint = getNewPoint();
+      const newScore = this.board.getScore(newPoint);
 
       this.totalScore += newScore;
       totalRounds--;
@@ -16,10 +58,13 @@ class Game {
 }
 
 const main = () => {
-  const game = new Game();
+  const board = new Board();
+  const game = new Game(board);
   const totalRounds = 2;
 
-  game.play(totalRounds);
+  return game.play(totalRounds);
 };
 
-exports.Game = Game;
+exports.main = main;
+exports.getNewPoint = getNewPoint;
+exports.Board = Board;
--- test/testDartGame.js
+++ test/testDartGame.js
@@ -1,10 +1,18 @@
 const assert = require('assert');
-const { Game } = require('../src/dartGame.js');
+const { Game, Board } = require('../src/dartGame.js');
 
-describe('Game', () => {
-  const game = new Game();
-  const actual = game.play(2);
-  it('Should give 10 as totalScore', () => {
-    assert.equal(actual, 10);
+describe('Board', () => {
+  it('Should give the distance from center (10,10)', () => {
+    const board = new Board();
+    const point = { x: 3, y: 5 }
+    const actual = board.distanceFromCenter(point);
+    assert.strictEqual(actual, 9);
+  });
+
+  it('Should give the distance from center when point (4,9)', () => {
+    const board = new Board();
+    const point = { x: 4, y: 9 }
+    const actual = board.distanceFromCenter(point);
+    assert.strictEqual(actual, 6);
   });
 });
